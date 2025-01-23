b17728f52c1edd60bdc1ed81653ac14f0d146e49
srdrflk <55311271+srdrflk@users.noreply.github.com>
2025-01-24 01:27:13 +0300

    Task2 implemented

---
+++ BinarySerializationMechanism/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerializationMechanism
+{
+    [Serializable]
+    public class Person : ISerializable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        
+        public Person() { } // Parameterless constructor for deserialization
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Special constructor used during deserialization
+        protected Person(SerializationInfo info, StreamingContext context)
+        {
+            // Retrieve data from SerializationInfo and assign it to properties
+            Name = info.GetString("Name");
+            Age = info.GetInt32("Age");
+        }
+
+        // This method is called during serialization
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Age", Age);
+        }
+    }
+}
---
+++ BinarySerializationMechanism/Program.cs
@@ -0,0 +1,32 @@
+﻿using BinarySerializationMechanism;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Serdar", 41);
+
+        string filePath = "person.dat";
+
+        // Serialize
+        IFormatter formatter = new BinaryFormatter();
+        using (FileStream stream = new FileStream(filePath, FileMode.Create))
+        {
+            formatter.Serialize(stream, person);
+        }
+        Console.WriteLine("Object Serialized.");
+
+        // Deserialize
+        Person deserializedPerson;
+        using (FileStream stream = new FileStream(filePath, FileMode.Open))
+        {
+            deserializedPerson = (Person)formatter.Deserialize(stream);
+        }
+        Console.WriteLine("Object Deserialized.");
+        Console.WriteLine($"Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+    }
+}
